8daa1bb599b4b4fe4787ca374a1d7fac6b715748
Sebastian Pulak <pulakosso@gmail.com>
2019-03-15 15:26:46 +0100

    add restructure

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
-let nav = '<nav><a href="../html/index.html"> Home  </a>' +
-          '<a href="../html/blog.html"> Blog  </a>' +
-          '<a href="../html/quiz.html"> Quiz  </a>' +
-          '<a href="../html/contact.html"> Kontakt  </a></nav>';
+let nav = '<nav><a href="index.html"> Home  </a>' +
+          '<a href="blog.html"> Blog  </a>' +
+          '<a href="quiz.html"> Quiz  </a>' +
+          '<a href="contact.html"> Kontakt  </a></nav>';
 
 let elem4 = document.querySelector(".navbar");
 
